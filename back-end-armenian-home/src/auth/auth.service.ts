eae4e818f2881b29892eecf1d5b1c29331cfe1e0
Sima <tarverdans15@gmail.com>
2023-03-11 19:35:20 +0300

    update auth

--- back-end-armenian-home/src/auth/auth.service.ts
+++ back-end-armenian-home/src/auth/auth.service.ts
@@ -1,12 +1,14 @@
 import { faker } from '@faker-js/faker';
 import { BadRequestException, Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { hash } from 'argon2';
 import { PrismaService } from 'src/prisma.service';
 import { AuthDto } from './auth.dto';
 
 @Injectable()
 export class AuthService {
-    constructor(private prismaService: PrismaService) {} // мы принимает базу данных, но для этого мы подключали его в auth.module.ts
+    [x: string]: any;
+    constructor(private prismaService: PrismaService, jwt: JwtService) {} // мы принимает базу данных, но для этого мы подключали его в auth.module.ts
 
     async register(dto: AuthDto) {
         const oldUser = await this.prismaService.user.findUnique({
@@ -31,9 +33,14 @@ export class AuthService {
     private async issueTokens(userId: number)   {
         const data = {id: userId}   
 
-        // const accessToken = this.jwtService.sign(data, {
-            
-        // })
+        const accessToken = this.jwt.sign(data, {
+            expiresIn: '1h',
+        })
+        const refreshToken = this.jwt.sign(data, {
+            expiresIn: '7d',
+        })
+        return {accessToken, refreshToken}
     }
-}
+    }
+
 
