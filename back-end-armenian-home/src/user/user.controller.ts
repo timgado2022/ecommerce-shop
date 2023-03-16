934218e3e2ade16fa6d95d432c84dabe316af035
Sima <tarverdans15@gmail.com>
2023-03-16 18:56:57 +0300

    update schema prisma | add return-user.object.ts and user.dto.ts, update user module | user controler | user service, insomnia queries in users(insomnia)

--- back-end-armenian-home/src/user/user.controller.ts
+++ back-end-armenian-home/src/user/user.controller.ts
@@ -1,7 +1,39 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, Param, Patch, Post, Put, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Auth } from 'src/auth/decorators/auth.decorator';
+import { CurrectUser } from 'src/auth/decorators/user.decorator';
+import { RefreshTokenDto } from 'src/auth/dto/refresh-token.dto';
+import { UserDto } from './user.dto';
 import { UserService } from './user.service';
 
-@Controller('user')
+@Controller('users')
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+  // get profile 
+  // update profile
+  // toggleFavorite
+  
+
+  @Get('profile')
+  @Auth()
+  async getProfile(@CurrectUser('id') id: number) {
+    return this.userService.byId(id);
+  } 
+
+  @UsePipes(new ValidationPipe())
+  @HttpCode(200) 
+  @Auth()
+  @Put('profile')
+  async getNewTokens(@CurrectUser('id') id: number, @Body() dto: UserDto) {
+    return this.userService.updateProfile(id,dto);
+  } 
+
+  
+  @HttpCode(200) 
+  @Auth()
+  @Patch('profile/favorites/:productId')
+  async toggleFavorite(@Param('productId') productId: string, @CurrectUser('id') id: number) {
+    return this.userService.toggleFavorite(id, +productId);
+  } 
+
 }
--- back-end-armenian-home/src/user/user.service.ts
+++ back-end-armenian-home/src/user/user.service.ts
@@ -1,4 +1,90 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
+import { Prisma } from '@prisma/client';
+import { hash } from 'argon2';
+import { NotFoundError } from 'rxjs';
+import { PrismaService } from 'src/prisma.service';
+import { returnUserObject } from './return-user.object';
+import { UserDto } from './user.dto';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+    constructor(private  prismaService: PrismaService) {} 
+
+    async byId(id: number, selectObject: Prisma.UserSelect = {}) {
+        const user = await this.prismaService.user.findUnique({
+            where: {
+                id
+            },
+            select: {
+                ...returnUserObject,
+                favorites: {
+                    select:{
+                        id: true,
+                        name: true,
+                        price: true,
+                        images: true,
+                        slug: true
+                    }
+                },
+                ...selectObject
+            }
+        })
+        if(!user){
+            throw new Error('User not found')
+        }
+
+        return user
+        
+    }
+
+    async updateProfile(id: number, dto: UserDto) {
+        const isSameUser = await this.prismaService.user.findUnique({
+            where:{
+                email: dto.email
+            }
+        })
+
+        if(isSameUser && id === isSameUser.id){ // где-то тут может быть ошибка
+            throw new BadRequestException('Email is already in use')
+            
+        const user = await this.byId(id)
+
+        return this.prismaService.user.update({
+            where: {
+                id
+            },
+            data: {
+                email:dto.email,
+                name: dto.name,
+                avatarPath: dto.avatarPath,
+                phone: dto.phone,
+                password: dto.password ? await hash(dto.password): user.password
+            }
+        })
+        } 
+    }
+
+
+    async toggleFavorite(userId: number, productId: number) {
+        const user = await this.byId(userId)
+
+        if(!user) throw new NotFoundException('User not found')
+
+        const isExists = user.favorites.some(product => product.id === productId)
+
+        await this.prismaService.user.update({ 
+            where: {
+                id: user.id
+            },
+            data:{
+                favorites: {
+                    [isExists ? 'disconnect' : 'connect']: {
+                        id: productId
+                    }
+                }
+            }
+        })
+
+        return "Success"
+    }
+}
