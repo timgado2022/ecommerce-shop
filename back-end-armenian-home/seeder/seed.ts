a518693ddb9853305cc687d3add8a30d48c144fe
Sima <tarverdans15@gmail.com>
2023-03-23 00:14:11 +0300

    add first product, test seeding, add command seed in package json

--- back-end-armenian-home/seeder/seed.ts
+++ back-end-armenian-home/seeder/seed.ts
@@ -1,37 +1,71 @@
-import { Category, PrismaClient, Product } from "@prisma/client";
+import { faker } from "@faker-js/faker";
+import { Category, Prisma, PrismaClient, Product } from "@prisma/client";
 import * as dotenv from "dotenv"
+import { products } from "./product/productList";
 
 dotenv.config();
 const prisma = new PrismaClient()
 
-const createProducts = async (quantity: number) => {
-    
-    const product : Product[] = []
+// const products: Prisma.ProductsCreateInput[] = [
+//     {
+//         name: "Armatfood. Компот из терна",
+//         slug: "armatfood-komot-iz-terna",
+//         description: "",
+//         price: 205,
+//         images: ["https://gurmanarmenia.ru/image/cache/catalog/irafoto/armatfud/tern-228x228.png"],
+//         category: "Напитки | Компоты | Соки"
+//     }
+// ]
+const createProducts = async () =>{
+    const products: Product[] = [];
 
-    console.log(`Created ${product.length} products`)
+    const product = await prisma.product.create({
+    data: {
+        name: "Kerakur. Икра баклажановая",
+        slug: "kerakur ikra",
+        description: "Икра баклажановая",
+        price: 250,
+        images: [],
+        category: {
+            create:{
+                name: "Овощная консервация",
+                slug: "ovoschnaya-konservatsiya", 
+            }
+        },
+        reviews: {
+            create:[
+                {
+                    rating: faker.datatype.number({ min: 1, max: 5 }),
+                    text: faker.lorem.paragraph(),
+                    user: {
+                        connect: {
+                            id: 1
+                        }
+                    }
 
+                },
+                {
+                    rating: faker.datatype.number({ min: 1, max: 5 }),
+                    text: faker.lorem.paragraph(),
+                    user: {
+                        connect: {
+                            id: 1
+                        }
+                    }
+                }
+            ]
+        }
+    }
+})
+    products.push(product)
 }
 
-// const createProducts = await prisma.product.create({
-//     data: {
-//         name: "Kerakur. Икра баклажановая",
-//         slug: "kerakur ikra",
-//         description: "Икра баклажановая",
-//         price: 250,
-//         images: [],
-//         category: {
-//             create:{
-//                 name: "Овощная консервация",
-//                 slug: "ovoschnaya-konservatsiya", 
-//             }
-//         }
-//     }
-// })
 
 async function main() {
     console.log("Start seeding...")   
-    await createProducts(10);   
-}
+   
+} 
+
 
 main()
     .catch(e => console.error(e))
