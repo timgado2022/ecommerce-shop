abe76e360eae5c93afde5938a1de1909f81229bb
Sima <tarverdans15@gmail.com>
2023-04-07 00:47:07 +0300

    add files: types(backend), fix: user.interface, auth.helper and api.interceptor

--- front-end-armenian-home/src/services/auth/auth.helper.ts
+++ front-end-armenian-home/src/services/auth/auth.helper.ts
@@ -1,16 +1,27 @@
+import { IAuthResponse, ITokens } from '@/store/user/user.interface'
 import Cookies from 'js-cookie'
+
+export const getAccessToken = () =>{
+    const accessToken = Cookies.get('accessToken')
+    return accessToken || null
+}
+
+export const getUserFromStorage = async () => {
+    return JSON.parse(localStorage.getItem('user') || '{}')
+}
 
 export const saveTokensStorage = (data: ITokens ) => {
     Cookies.set('accessToken', data.accessToken)
     Cookies.set('refreshToken', data.refreshToken)
 }
 
-export const removeTokensStorage = () => {
-    Cookies.remove('accessToken')
-    Cookies.remove('refreshToken')
-}
 
 export const saveToStorage = (data: IAuthResponse) => {
     saveTokensStorage(data)
     localStorage.setItem('user', JSON.stringify(data.user))
+}
+
+export const removeTokensStorage = () => {
+    Cookies.remove('accessToken')
+    Cookies.remove('refreshToken')
 }
